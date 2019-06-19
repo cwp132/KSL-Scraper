8ccd8dfc53a5439abc4f0b4d968250766fe51c5a
cwp132 <44931480+cwp132@users.noreply.github.com>
2019-06-19 14:31:11 -0600

    port fix

--- server.js
+++ server.js
@@ -8,8 +8,7 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 8080;
-
+   var PORT = process.env.PORT || 3000;
 // Initialize Express
 
 var app = express();
